1271ff2096c1f2602d16b35fed7a3d344369d288
braimanm <braimanm@gmail.com>
2013-12-17 18:32:57 +0000

    pom file changes and forgotten artefact was added

---
+++ page-component/src/test/java/test/com/PageComponent/DataSet2.java
@@ -0,0 +1,18 @@
+package test.com.PageComponent;
+
+import java.util.List;
+
+import ui.auto.core.components.WebComponent;
+import ui.auto.core.data.DataPersistence;
+import ui.auto.core.data.generators.Data;
+
+public class DataSet2 extends DataPersistence{
+	@Data(alias="alias1")
+	WebComponent comp4;
+	@Data("component5")
+	WebComponent comp5;
+	@Data(alias="alias-l1")
+	List<WebComponent> list3;
+	@Data(alias="array-alias",value="array-value",n=10)
+	WebComponent[] array1;
+}

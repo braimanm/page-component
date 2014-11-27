1339c4085d79a9af54a3d67b780fa46886ca424a
braimanm <braimanm@gmail.com>
2014-11-27 05:31:49 +0000

    Adding Java documentation and reformatting the code

--- page-component/src/main/java/ui/auto/core/data/DataPersistence.java
+++ page-component/src/main/java/ui/auto/core/data/DataPersistence.java
@@ -54,6 +54,11 @@ public class DataPersistence {
 	
 	protected DataAliases aliases;
 	
+	/**
+	 * Returns alias for the given key
+	 * @param key key for the alias 
+	 * @return local alias
+	 */
 	public String getDataAlias(String key){
 		if (aliases!=null){
 			return aliases.get(key).toString();
@@ -61,6 +66,12 @@ public class DataPersistence {
 		return null;
 	}
 	
+	/**
+	 * Set local and global alias
+	 * This method duplicates local alias to the global alias store to be resolved in any other inherited {@link DataPersistence} object
+	 * @param key provides the key for the alias to store
+	 * @param value value to store for the alias
+	 */
 	public void setDataAlias(String key,String value){
 		if (aliases==null){
 			aliases=new DataAliases();
@@ -76,17 +87,21 @@ public class DataPersistence {
 		return xstream;
 	}
 	
-	protected static XStream initXstream(Class<?> clz){
+	private static XStream initXstream(Class<?> clz){
 		XStream xstream=getXStream();
 		xstream.processAnnotations(clz);
 		return xstream;
 	}
 	
+	/**
+	 * This method serializes this object to the given XML string
+	 * @return XML representation of this object 
+	 */
 	public String toXML(){
 		String header="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n";
 		XStream xstream=initXstream(this.getClass());
 		
-		// If this class extends PageObject and it initialised with context 
+		// If this class extends PageObject and it initialized with context 
 		// then all the WebComponent fields of this class are CGLIB proxies and by default xml node
 		// is marked with attribute class. This will remove class attribute from the xml node 
 		if ( PageObject.class.isAssignableFrom(this.getClass()) && 
@@ -97,6 +112,10 @@ public class DataPersistence {
 		return header + xml;
 	}
 	
+	/**
+	 * This method serializes this object to the given file
+	 * @param filePath file path to serialize this object
+	 */
 	public void toFile(String filePath){
 		
 		FileOutputStream fos=null;
@@ -121,25 +140,48 @@ public class DataPersistence {
 		
 	}
 	
-		
+	/**
+	 * This method deserialize given XML string to the object 	
+	 * @param xml XML string which represents deserialized object
+	 * @param forClass class to deserialize 
+	 * @return deserialized object
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T fromXml(String xml,Class<T> forClass){	
 		DataPersistence data=(DataPersistence) initXstream(forClass).fromXML(xml);
 		return (T) data;
 	}
 	
+	/**
+	 * This method deserialize file represented by URL to the object
+	 * @param url URL pointer to the file to be deserialized
+	 * @param forClass class to deserialize
+	 * @return deserialized object
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T fromURL(URL url,Class<T> forClass){
 		DataPersistence data=(DataPersistence) initXstream(forClass).fromXML(url);
 		return  (T) data;
 	}
 	
+	/**
+	 * This method deserialize {@link InputStream} to the object
+	 * @param inputStream input stream to deserialize from
+	 * @param forClass class to deserialize
+	 * @return deserialized object
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T fromInputStream(InputStream inputStream,Class<T> forClass){
 		DataPersistence data=(DataPersistence) initXstream(forClass).fromXML(inputStream);
 		return  (T) data;
 	}
 	
+	/**
+	 * This method deserialize given resource file to the object 
+	 * @param resourceFile resource file to deserialize from
+	 * @param forClass class to deserialize
+	 * @return deserialized object
+	 */
 	public static <T> T fromResource(String resourceFile,Class<T> forClass){
 		URL url=Thread.currentThread().getContextClassLoader().getResource(resourceFile);
 		if (url==null){
@@ -148,6 +190,12 @@ public class DataPersistence {
 		return fromURL(url, forClass);
 	}
 	
+	/**
+	 * This method deserialize given file to the object 
+	 * @param filePath file path to deserialize from
+	 * @param forClass class to deserialize
+	 * @return deserialized object
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T fromFile(String filePath,Class<T> forClass){
 		File file=new File(filePath);
@@ -158,18 +206,35 @@ public class DataPersistence {
 		return (T) data;
 	}
 	
+	/**
+	 * Creates exact copy of the given object 
+	 * @param object Object to copy
+	 * @return copy of the given object
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T deepCopy(T object){
 		XStream xstream=getXStream();
 		return (T) xstream.fromXML(xstream.toXML(object));
 	}
 	
+	/**
+	 * Copying one object to another
+	 * @param source object to copy from
+	 * @param target object to copy to
+	 */
 	public static void deepCopy(Object source,Object target){
 		XStream xstream=getXStream();
 		String xml=xstream.toXML(source);
 		xstream.fromXML(xml,target);
 	}
 	
+	/**
+	 * Instantiates given class without invoking any declared constructor. 
+	 * Class members are not instantiated and they will equal to null even if they declared with assignments.
+	 * 
+	 * @param forClass class to instantiate
+	 * @return Instantiated object for given class
+	 */
 	public static <T> T instantiateClass(Class<T> forClass){
 		if (forClass.isMemberClass()) {
 			return fromXml("<" + forClass.getName().replace("$","_-") + "/>", forClass);
@@ -178,11 +243,21 @@ public class DataPersistence {
 		}
 	}
 	
+	/**
+	 * This method will generate XML template from this class
+	 * @return XML generated from this class 
+	 * @throws Exception
+	 */
 	public String generateXML() throws Exception{
 		DataSetGenerator.getInstance().generateData(this,true);
 		return toXML();
 	}
 	
+	/**
+	 * This method will generate XML file from this class
+	 * 
+	 * @return XML generated from this class 
+	 */
 	public String generateData(){
 		return DataSetGenerator.getInstance().generateDataSet(this);
 	}
